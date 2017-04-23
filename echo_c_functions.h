3fba08dc7391dee0298e1b9614c22f9f07627098
AshWije <ashiniwije126@gmail.com>
2017-04-22 21:35:54 -0500

    Create echo_c_functions.h

---
+++ echo_c_functions.h
@@ -0,0 +1,8 @@
+// client_functions.h
+// header file that contains the declarations of the functions implemented in client.c
+
+// declaration of the error function
+void error(const char *);
+
+//declaration of the makeSocket function
+int makeSocket(int);
