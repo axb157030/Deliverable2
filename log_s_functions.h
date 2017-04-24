1af4a5a0002515f3610486e5f5d5d289f99c0ce5
ShindoKazumi <tearaeng@yahoo.com>
2017-04-23 19:42:39 -0500

    Update log_s_functions.h

--- log_s_functions.h
+++ log_s_functions.h
@@ -1,1 +1,10 @@
-// for future use
+//Header file that contain the declarations of functions used
+
+//Protoype for displaying error 
+void error(const char *);
+
+//Prototype for making socket
+void makeSocket();
+
+//Prototype for writing to file
+void fileWrite();
