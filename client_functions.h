f6cbe33307521bbda1167ede86126df54142c81b
JacobFact <42regime@gmail.com>
2017-04-26 10:33:43 -0500

    Delete client_functions.h

--- client_functions.h
+++
@@ -1,8 +0,0 @@
-// client_functions.h
-// header file that contains the declarations of the functions implemented in client.c
-
-// declaration of the error function
-void error(const char *);
-
-//declaration of the makeSocket function
-int makeSocket(int);
